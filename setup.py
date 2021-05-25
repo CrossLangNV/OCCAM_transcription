31e182518ed4826e33e6b27c9f3e5f91ceddda4a
laurens <laurensmeeus93@gmail.com>
2021-05-25 21:53:00 +0200

    Trying to get packaging to work

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from setuptools import setup
 
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
 with open('requirements.txt') as f:
     required = f.read().splitlines()
+
 
 setup(
     name='OCCAM-transcription', # the name of the package
@@ -10,6 +15,8 @@ setup(
     author='Laurens Meeus',
     author_email='laurens.meeus@crosslang.com',
     description="Python wrapper for the XML's regarding the transcription",
+    long_description=long_description,
     # scripts=[''], # the launcher script
     install_requires=required,  # external packages as dependencies
 )
+
